82530daa143231b855ea31307392f7229c788b08
chippy-1914 <50676688+chippy-1914@users.noreply.github.com>
2021-06-05 16:58:27 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, {  useState} from 'react';
+import { StyleSheet, Text, View,ScrollView } from 'react-native';
+
 import Regiform from './components/Form';
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCIHDCsSmctBT_eagJnFqttRGFd8xI6_VU",
+  authDomain: "shoeshop-75160.firebaseapp.com",
+  projectId: "shoeshop-75160",
+  storageBucket: "shoeshop-75160.appspot.com",
+  messagingSenderId: "789844170150",
+  appId: "1:789844170150:web:a2fe9a6cf12cb306356b67"
+};
+// Initialize Firebase
+if(firebase.apps.length===0){
+firebase.initializeApp(firebaseConfig);
+}
+
 
 
 export default function App() {
